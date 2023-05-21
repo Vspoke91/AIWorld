a7d7c74fd611a8c8280868c8d7ed7c4e32140997
Victor R <40780266+Vspoke91@users.noreply.github.com>
2023-05-21 10:51:21 -0400

    Revert "Feat/sorting"
    This reverts commit b9a6667ab8aace2f0147b9afe35b9f121b60657b.

---
+++ src/App.jsx
@@ -0,0 +1,116 @@
+import { useState } from 'react';
+import './App.css'
+import contentData from './sitesDataBase' 
+
+function App() {
+
+  let [displayShowDiv, setDisplayShowDiv] = useState(false);
+
+  //States for Showdiv Variables
+  let [showDivName, setShowDivName] = useState("*Name*");
+  let [showDivLogoPath, setShowDivLogoPath] = useState("*Path*");
+  let [showDivDescription, setShowDivDescription] = useState("*Description*");
+  let [showDivTag, setShowDivTag] = useState({});
+  let [showDivCategory,setShowDivCategory] = useState([]);
+  let [showDivWebLink, setShowDivWebLink] = useState("")
+
+  let ContentElements = () => {
+
+    let contentArray = new Array();
+
+    for(let i = 0; i < contentData.length; i++){
+
+      let siteData = contentData[i];
+
+      contentArray.push(
+        <div key={i} className='content-div' onClick={() => {loadDisplayShowDiv(i); setDisplayShowDiv(true);}}>
+          <span className='tag' style={{backgroundColor: siteData.tagType.color}}>{siteData.tagType.text}</span>
+          <h2>{siteData.title}</h2>
+          <div className='img-div'>
+            <img src={siteData.logo} alt={siteData.title + " Logo"}/>
+          </div>
+          <div className='category-div'>
+            {siteData.category.map((category, index) => 
+              <span key={index}>{category}</span>
+            )}
+          </div>
+          <div className='description'>
+            <p>{siteData.description}</p>
+          </div>
+        </div>
+        );
+    }
+  
+    return(contentArray);
+  }
+
+  let loadDisplayShowDiv = (index) =>{
+    setShowDivName(contentData[index].title);
+    setShowDivLogoPath(contentData[index].logo);
+    setShowDivDescription(contentData[index].description);
+    setShowDivTag(contentData[index].tagType);
+    setShowDivCategory(contentData[index].category);
+    setShowDivWebLink(contentData[index].webLink);
+  }
+
+  return (
+    <>
+      <div className='underDevelopmentDiv'>
+        <p>🚧 Website under development, features are limited 🚧</p>
+      </div>
+      <header>
+          <h1>AI World</h1>
+          <div className='quote-div'>
+            <p>Artificial intelligence is the next stage in the evolution of human beings</p>
+            <span>Stephen Hawking</span>
+          </div>
+      </header>
+      
+      <main>
+        <ContentElements />
+        <div 
+          className='background-blur'
+          style={{display: displayShowDiv ? "block" : "none"}} 
+          onClick={()=> setDisplayShowDiv(false) }
+        ></div>
+
+        <div
+        className={`show-Div${displayShowDiv ? " show-animate" : ""}`}
+        style={{display: displayShowDiv ? "block" : "none"}}>
+            <div className='show-title-div'>
+              <h2>
+                <a href={showDivWebLink}>{showDivName}</a>
+              </h2>
+              <span className='tag' style={{backgroundColor: showDivTag.color}}>{showDivTag.text}</span>
+            </div>
+            <div className='img-div'>
+                <img src={showDivLogoPath} alt={showDivName + " Logo"}/>
+            </div>
+            <div className='category-div'>
+              {showDivCategory.map((category, index)=><span key={index}>{category}</span>)}
+            </div>
+            <div className='description'>
+            <p>{showDivDescription}</p>
+          </div>
+        </div>
+      </main>
+
+      <footer>
+        <div className='opensource-div'>
+          <div>
+            <p>Open Source Code</p>
+            <a href='https://github.com/Vspoke91/AIWorld'><img src="/img/logos/GitHub.svg" alt="Github Logo"/></a>
+          </div>
+        </div>
+        <div className='velta-logo-div'>
+          <a href="https://www.veltaproject.com"><img src="https://veltaproject.com/VeltaLogo.png" alt="Velta Logo"/></a>
+        </div>
+        <div className='copyright-div'>
+          <p>© 2023 AI World, VeltaProject</p>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default App
--- src/sitesDataBase.js
+++ src/sitesDataBase.js
@@ -11,32 +11,13 @@ const tags = {
     }
 }
 
-export const categories = {
-    chat: {
-        text: 'Chat',
-        color: 'rgb(1, 71, 11)'
-    },
-    login: {
-        text: 'Login equired',
-        color: 'rgb(141, 3, 3)'
-    },
-    discord: {
-        text: 'Discord',
-        color: 'rgb(141, 3, 3)'
-    },
-    graphics: {
-        text: 'Images & Graphics',
-        color: 'rgb(141, 3, 3)'
-    },
-}
-
 const items = [
     {
         title: "ChatGPT",
         webLink: "https://chat.openai.com/",
         logo: LOGOS_PATH+"ChatGPT.svg",
         description: "ChatGPT is an AI-powered chatbot that can hold conversations with users on a wide range of topics.",
-        category: [categories.chat.text,  categories.login.text],
+        category: ['Chat', 'Login'],
         tagType: tags.free,
     },
     {
@@ -44,7 +25,7 @@ const items = [
         webLink: "https://www.bluewillow.ai/",
         logo: LOGOS_PATH+"BlueWillow.png",
         description: "AI-powered tool that can generate a wide variety of graphics based on user input.",
-        category: [categories.graphics.text, categories.login.text, categories.discord.text],
+        category: ['Images & Graphics', 'Login', 'Discord'],
         tagType: tags.free
     },
     {
@@ -52,7 +33,7 @@ const items = [
         webLink: "https://www.midjourney.com/",
         logo: LOGOS_PATH+"Midjourney.png",
         description: "Midjourney is a technology that uses artificial intelligence to generate images from natural language descriptions, which are called 'prompts'.",
-        category: [categories.graphics.text, categories.login.text, categories.discord.text],
+        category: ['Images & Graphics', 'Login', 'Discord'],
         tagType: tags.free
     },
     {
@@ -60,7 +41,7 @@ const items = [
         webLink: "https://deepai.org/",
         logo: LOGOS_PATH+"DeepAI.png",
         description: "AI Chat is an AI chatbot that writes text. You can use it to write stories, messages, or programming code. You can use the AI chatbot as a virtual tutor in almost any subject.",
-        category: [categories.chat.text, categories.graphics.text],
+        category: ['Chat', 'Images & Graphics', 'No-login'],
         tagType: tags.free
     },
     {
@@ -68,7 +49,7 @@ const items = [
         webLink: "https://hotpot.ai/",
         logo: LOGOS_PATH+"Hotpot.png",
         description: "Hotpot helps you create amazing graphics, pictures, and writing.",
-        category: [categories.graphics.text],
+        category: ['Images & Graphics', 'No-login'],
         tagType: tags.free
     },
 ]
