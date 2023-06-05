c2c5077c8caa93e363e1df30ea2f58f93fbd12d3
Vspoke91 <victorvjro91@gmail.com>
2023-06-04 21:45:44 -0400

    ADD: Arrow SVG

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,12 @@
 import { Outlet, Link } from "react-router-dom";
 import './Layout.css'
+import { ArrowSVG } from '../../assets/CustomIcons';
 
-const Header = () => {
-    return (
-      <>
+const Layout = () => {
+
+  return (
+    <>
+      <div id="header-holder">
         <header className='qs__flex_column'>
             <Link to='/' className='logo qs__flex_column __flex_center'>
                 <img src='/img/logos/AI-World-Small.png'/>
@@ -21,29 +24,31 @@ const Header = () => {
                 <span>Stephen Hawking</span>
             </div>
         </header>
+        <div className="slider-control"><ArrowSVG /></div>
+      </div>
 
-        <div className="qs__sidebar_spacing qs__flex_column qs__height_full_percent qs_scroll_y">
-          <main>
-            <Outlet/>
-          </main>
+      <div className="qs__sidebar_spacing qs__flex_column qs__height_full_percent qs_scroll_y">
+        <main>
+          <Outlet/>
+        </main>
 
-          <footer>
-              <div className='opensource-div'>
-                  <div>
-                    <p>Open Source Code</p>
-                    <a href='https://github.com/Vspoke91/AIWorld/blob/master/README.md'><img src="/img/logos/GitHub.svg" alt="Github Logo"/></a>
-                  </div>
-              </div>
-              <div className='velta-logo-div'>
-                <a href="https://www.veltaproject.com"><img src="https://veltaproject.com/VeltaLogo.png" alt="Velta Logo"/></a>
-              </div>
-              <div className='copyright-div'>
-                <p>© 2023 AI World, VeltaProject</p>
-              </div>
-          </footer>
-        </div>
-      </>
-    )
-  };
+        <footer>
+            <div className='opensource-div'>
+                <div>
+                  <p>Open Source Code</p>
+                  <a href='https://github.com/Vspoke91/AIWorld/blob/master/README.md'><img src="/img/logos/GitHub.svg" alt="Github Logo"/></a>
+                </div>
+            </div>
+            <div className='velta-logo-div'>
+              <a href="https://www.veltaproject.com"><img src="https://veltaproject.com/VeltaLogo.png" alt="Velta Logo"/></a>
+            </div>
+            <div className='copyright-div'>
+              <p>© 2023 AI World, VeltaProject</p>
+            </div>
+        </footer>
+      </div>
+    </>
+  )
+};
   
-  export default Header;
+ export default Layout;
