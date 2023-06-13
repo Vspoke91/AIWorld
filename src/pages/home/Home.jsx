18440ce2f98a42a4f0c1eec82a3fd219a60bf6cd
Vspoke91 <victorvjro91@gmail.com>
2023-06-12 20:40:19 -0400

    ADD: Layout for Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,65 @@
 import './Home.css'
 
 function Home() {
-    return (<>Home Page</>);
+    return (
+        <>
+            <div>
+                <h2>Feature Websites</h2>
+                <div className='qs__grid_row qs_scroll_y'>
+                    
+                    <div>
+                        <img/>
+                        <h3>AI</h3>
+                    </div>
+                    <div>
+                        <img/>
+                        <h3>AI</h3>
+                    </div>
+                    <div>
+                        <img/>
+                        <h3>AI</h3>
+                    </div>
+                    <div>
+                        <img/>
+                        <h3>AI</h3>
+                    </div>
+                    <div>
+                        <img/>
+                        <h3>AI</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h1>AI World</h1>
+                <p>Explore a variety of tools that use AI to generate amazing graphics, write engaging texts, and chat with you on any topic</p>
+            </div>
+            <div>
+                <h2>Development</h2>
+                <div>
+                    <div>
+                        <h3>VErion</h3>
+                        <p>Description</p>
+                    </div>
+                    <div>
+                        <h3>VErion</h3>
+                        <p>Description</p>
+                    </div><div>
+                        <h3>VErion</h3>
+                        <p>Description</p>
+                    </div><div>
+                        <h3>VErion</h3>
+                        <p>Description</p>
+                    </div><div>
+                        <h3>VErion</h3>
+                        <p>Description</p>
+                    </div><div>
+                        <h3>VErion</h3>
+                        <p>Description</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Home;
