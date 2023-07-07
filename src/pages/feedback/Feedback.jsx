3ca14416d3d1d9a542c3415cda054e39294e3a9f
Vspoke91 <victorvjro91@gmail.com>
2023-07-07 17:03:57 -0400

    ADD: Layout for Feedback

--- src/pages/feedback/Feedback.jsx
+++ src/pages/feedback/Feedback.jsx
@@ -2,7 +2,14 @@
 import './Feedback.css'
 
 function Feedback() {
-    return (<>Feedback Page</>);
+    return (
+    <>
+        <h1>Send us your feedback!</h1>
+        <p>Feedback is important to us because it provides our developers with valuable information to improve the experiences of other users, including yourself.</p>
+        <form>
+            
+        </form>
+    </>);
 }
 
 export default Feedback;
