cdde2f95559bc7e57622ddde9a5ece2d8312257c
Vspoke91 <victorvjro91@gmail.com>
2023-07-12 22:28:40 -0400

    ADD: small change

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,7 +2,30 @@
 import './About.css'
 
 function About() {
-    return (<>About Page</>);
+    return (
+        <>
+            <div>
+                <h1>About AI World</h1>
+                <div><img src='/img/logos/AI-World-Large.png'/>+<img src='https://veltaproject.com/VeltaLogo.png'/></div>
+                <p>Mission</p>
+            </div>
+
+            <div>
+                <h2>Media</h2>
+                <div><span>instagram</span><span>Github</span><span>VeltaProject</span></div>
+            </div>
+
+            <div>
+                <h2>Contact</h2>
+                <div><a>aiworld@veltaproject.com</a><span></span></div>
+            </div>
+
+            <div>
+                <h2>Creator</h2>
+                <p>Victor Romero</p>
+            </div>
+        </>
+    );
 }
 
 export default About;
