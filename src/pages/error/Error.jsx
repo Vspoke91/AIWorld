43659a6cbe51c82d784018ce2de314a274168843
Vspoke91 <victorvjro91@gmail.com>
2023-08-08 22:33:19 -0400

    ADD: layout elements for error page

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom';
+
 function Error(){
-    return(<>Error 404 Page</>)
+    return(
+        <>
+            <div>
+                <h1>404 Error</h1>
+                <p>Page not found!</p>
+                <Link to="/"><button>Go back to Home</button></Link>
+            </div>
+        </>
+    );
 }
 
 export default Error;
