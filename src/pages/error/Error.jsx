376e96b052467792ecaed5d075cc31c415349ea9
Vspoke91 <victorvjro91@gmail.com>
2023-08-09 22:49:45 -0400

    ADD: useLocation for page not found

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,11 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Error(){
+
     return(
         <>
             <div>
                 <h1>404 Error</h1>
-                <p>Page not found!</p>
+                <p><b>&quot;{useLocation().pathname.slice(1)}&quot;</b> not found!</p>
                 <Link to="/"><button>Go back to Home</button></Link>
             </div>
         </>
