db9b1dac9c07874859a5e96ee1ed44e26804ff40
Vspoke91 <victorvjro91@gmail.com>
2023-08-23 20:50:21 -0400

    CHNG: data auto refresh new structure

--- src/pages/database/Database.jsx
+++ src/pages/database/Database.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import PropTypes from 'prop-types';
+import PropTypes, { func } from 'prop-types';
 import './Database.css'
 
 import { default as database, authentication } from '../../assets/database/firebase'
@@ -19,6 +19,9 @@ function Database (){
 
 function UserUI (){
 
+    let [collectionsData, setCollectionsData] = useState(null);
+
+
     const [userInfo, setUserInfo] = useState(null)
     const [collectionDisplayData, setCollectionDisplayData] = useState({
         collectionName: null,
@@ -31,17 +34,47 @@ function UserUI (){
 
     //loading information for component
     useEffect(() => {
-        (async function(){setUserInfo(await authentication.getUserInfo())})()
+        (async function(){setUserInfo(await authentication.getUserInfo())})();
 
-        displayCollectionChangeHandler("websites")
+        (async function (){
+            setCollectionsData({
+                websites: await database.getWebsites(),
+                tag: await database.getTags(),
+                categories: await database.getCategories(),
+            });
+
+            setCollectionDisplayData({
+                collectionName: 'websites',
+                collection: await database.getWebsites(),
+                nameFieldRef: 'name', 
+                logoUrlFieldRef: 'logoUrl'
+            }); 
+        })();
     },[])
 
     //start auto-refresh
     useEffect(() => {
         const refreshTime = 1000 // 1 sec = 1,000
 
+        const refreshVariable = async (value) => {
+            switch(value){
+                case ('websites'):{
+                    const data = await database.getWebsites();
+                    setCollectionsData(prev => ({...prev, websites: data}));
+                break;}
+                case ('categories'):{
+                    const data = await database.getCategories();
+                    setCollectionsData(prev => ({...prev, categories: data}));
+                break;}
+                case ('tag'):{
+                    const data = await database.getTags();
+                    setCollectionsData(prev => ({...prev, tag: data}));
+                break;}
+            }
+        }
+
         const intervalId = setInterval(() => {
-            displayCollectionChangeHandler(collectionDisplayData.collectionName);
+            refreshVariable(collectionDisplayData.collectionName);
         }, refreshTime);
       
         // Clear the interval when the component unmounts
@@ -60,49 +93,35 @@ function UserUI (){
         return elementArray;
     }
 
-    let currentAsyncTask = null;
-
-    const displayCollectionChangeHandler = async (value, force) =>{
-
-        if (currentAsyncTask && !force) {
-            return;
-        }
-
-        currentAsyncTask = (async () => {
-            try{
-                switch(value){
-                    case ('websites'):
-                        setCollectionDisplayData({
-                            collectionName: 'websites',
-                            collection: await database.getWebsites(), 
-                            nameFieldRef: 'name', 
-                            logoUrlFieldRef: 'logoUrl'
-                        }); 
-                    break;
-                    case ('categories'):setCollectionDisplayData({
-                            collectionName: 'categories',
-                            collection: await database.getCategories(), 
-                            nameFieldRef: 'text', 
-                            logoUrlFieldRef: 'logoUrl'
-                        });
-                    break; 
-                    case ('tag'): setCollectionDisplayData({
-                            collectionName: 'tag',
-                            collection: await database.getTags(), 
-                            nameFieldRef: 'text', 
-                            logoUrlFieldRef: 'logoUrl'
-                        });
-                    break;
-                }
-            } finally {
-                currentAsyncTask = null;
-            }
-        })();
-
-        try {
-            await currentAsyncTask;
-        } catch (error) {
-            console.error(error)
+    const displayCollectionChangeHandler = (value) =>{
+        switch(value){
+            case ('websites'):{
+                setCollectionDisplayData((prev) => ({
+                    ...prev,
+                    collectionName: 'websites',
+                    collection: collectionsData[value], 
+                    nameFieldRef: 'name', 
+                    logoUrlFieldRef: 'logoUrl'
+                })); 
+            break;}
+            case ('categories'):{
+                    setCollectionDisplayData((prev) => ({
+                        ...prev,
+                        collectionName: 'categories',
+                        collection: collectionsData[value], 
+                        nameFieldRef: 'text', 
+                        logoUrlFieldRef: 'logoUrl'
+                    }));
+            break;}
+            case ('tag'):{
+                setCollectionDisplayData((prev) => ({
+                        ...prev,
+                        collectionName: 'tag',
+                        collection: collectionsData[value], 
+                        nameFieldRef: 'text', 
+                        logoUrlFieldRef: 'logoUrl'
+                    }));
+            break;}
         }
     }
 
@@ -116,7 +135,7 @@ function UserUI (){
         <>
             <div className='qs__flex_row'>
                 <div className='qs__flex_column'> 
-                    <select className='' defaultValue="websites" onChange={(event) => {displayCollectionChangeHandler(event.target.value, true)}}>
+                    <select className='' defaultValue="websites" onChange={(event) => {displayCollectionChangeHandler(event.target.value)}}>
                         <option value="websites">Websites</option>
                         <option value='tag'>Tag</option>
                         <option value='categories'>Categories</option>
