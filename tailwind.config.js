9b3c98be78babf8310ede2ada5eec8513c21f677
Victor R. <40780266+Vspoke91@users.noreply.github.com>
2023-10-30 02:43:52 +0000

    ADD: custom color for highlight

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        custom_highlight: "#F59E0B",
+      },
       fontFamily: {
         fontawesome: ["Font Awesome 6 Free", "sans-serif"],
       },
