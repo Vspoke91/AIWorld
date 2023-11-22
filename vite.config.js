12e9f072efdf676bb2e0290b5355e7c6486c5838
Victor R <victorvjro91@gmail.com>
2023-11-22 08:18:58 -0500

    ADD: host command to package.json and remove server host configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true
-  },
   resolve: {
     alias: {
-      '@':  path.resolve('./')
+      "@": path.resolve("./"),
     },
   },
-})
+});
