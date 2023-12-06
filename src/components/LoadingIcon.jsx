cd4124f3f0e607e11ed09f1717b17b7392d21142
Victor R <victorvjro91@gmail.com>
2023-12-05 21:26:00 -0500

    CHNG: LoadingIcon component to include text prop

--- src/components/LoadingIcon.jsx
+++ src/components/LoadingIcon.jsx
@@ -5,15 +5,24 @@ import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 // React imports
 import PropTypes from "prop-types";
 
-export default function Default({ textSize = "text-4xl" }) {
+export default function Default({
+  text,
+  textSize = "text-4xl",
+  iconSize = "text-6xl",
+}) {
   return (
-    <FontAwesomeIcon
-      icon={faSpinner}
-      className={`animate-[spin_1.5s_linear_infinite] text-4xl ${textSize}`}
-    />
+    <div className="flex items-center">
+      <FontAwesomeIcon
+        icon={faSpinner}
+        className={`animate-[spin_1.5s_linear_infinite] ${iconSize}`}
+      />
+      <span className={`font-mono ${textSize} pl-2`}>{text}</span>
+    </div>
   );
 }
 
 Default.propTypes = {
   textSize: PropTypes.string,
+  text: PropTypes.string.isRequired,
+  iconSize: PropTypes.string,
 };
