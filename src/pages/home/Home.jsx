13f9adf3427a42d86bb8abae95fb60940ad3c94f
Victor R <victorvjro91@gmail.com>
2023-12-13 15:45:00 -0500

    CHNG: use HomeIntro Component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,79 +1,22 @@
 // React imports
 import { useEffect, useState } from "react";
 import { formatDistanceToNow } from "date-fns";
-import { Link } from "react-router-dom";
+
 // Components imports
 import Featured from "./HomeFeatured";
+import Introduction from "./HomeIntro";
 
 function Home() {
   return (
     <>
-      <div id="FeaturedSection">
+      <div>
         <Featured />
       </div>
-      <div id="IntroductionSection">
+      <div className="my-[10vh] flex items-center justify-center gap-14">
         <Introduction />
-        <span className="hidden-tittle">AI World</span>
       </div>
       <div id="DevelopmentSection">
         <Development />
-      </div>
-    </>
-  );
-}
-
-function Introduction() {
-  return (
-    <>
-      <div className="description-holder">
-        <h1>
-          The best place to find AI tools to create graphics, code, and chat.
-        </h1>
-        <p>
-          AI-World is a <strong>AI tools hub</strong> to facilitate the search
-          of tools in the web. Explore a variety of tools that use AI to
-          generate amazing graphics, write engaging texts, and chat with you on
-          any topic.
-        </p>
-        <Link to="/search">
-          <button>Search Websites</button>
-        </Link>
-      </div>
-
-      <div className="items-holder">
-        <div className="item item-graphic">
-          <img src="https://firebasestorage.googleapis.com/v0/b/ai-world-eae98.appspot.com/o/Logos%2FBlueWillow.png?alt=media&token=da9910cf-6c39-4436-a72c-380411f9f46c" />
-          <a href="https://www.bluewillow.ai/" target="_black" rel="noreferrer">
-            <span>BlueWillow</span>
-            <p>
-              State-of-the-art artificial intelligence{" "}
-              <strong>image generator</strong>.
-            </p>
-          </a>
-        </div>
-        <div className="item item-chat">
-          <img src="https://firebasestorage.googleapis.com/v0/b/ai-world-eae98.appspot.com/o/Logos%2FChatGPT.svg?alt=media&token=2f18a303-41ec-492e-ba79-3c31b32a4753" />
-          <a href="https://chat.openai.com/" target="_blank" rel="noreferrer">
-            <span>ChatGPT</span>
-            <p>
-              Natural language AI <strong>chatbot</strong> based on
-              OpenAI&apos;s GPT-3.5.
-            </p>
-          </a>
-        </div>
-        <div className="item item-code">
-          <img src="/img/logos/Phind.png" />
-          <a
-            href="https://firebasestorage.googleapis.com/v0/b/ai-world-eae98.appspot.com/o/Logos%2FPhind.png?alt=media&token=51c09dd3-22ea-4b62-b5b5-b121882a69ce"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span>Phind</span>
-            <p>
-              Generative AI search engine for <strong>developers</strong>.
-            </p>
-          </a>
-        </div>
       </div>
     </>
   );
