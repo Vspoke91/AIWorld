d14ca856bff4b61f09407b41eb8c183da7243aa5
Victor R <victorvjro91@gmail.com>
2024-01-02 21:31:30 -0500

    CHNG: sorting checkboxes Styles

--- src/pages/search/SortingCheckboxes.jsx
+++ src/pages/search/SortingCheckboxes.jsx
@@ -35,29 +35,33 @@ export default function Default({ activeCategoriesState }) {
 
   return (
     <>
+      <div className="absolute left-0 top-[calc(100%-10px)] z-[-1] hidden flex-col overflow-hidden rounded-b-lg bg-neutral-500 pb-1 pt-[10px] hover:flex peer-hover:flex">
+        <span className="mx-2 text-sm font-bold">Most have:</span>
+        <ul className="contents">
           {categories?.length ? (
-          Object.keys(categories).map((key) => (
-            <li key={key} className="contents">
-              <label className="">
-                <input
-                  type="checkbox"
-                  name={categories[key].text}
-                  checked={activeCategories.includes(categories[key].text)}
-                  onClick={activateCategoryHandler}
-                />
-
-                {categories[key].text}
-              </label>
-            </li>
-          ))
-        ) : (
+            Object.keys(categories).map((key) => (
+              <li key={key} className="contents">
+                <label className="inline cursor-pointer whitespace-nowrap px-2 hover:bg-black">
+                  <input
+                    type="checkbox"
+                    name={categories[key].text}
+                    className="mr-1"
+                    checked={activeCategories.includes(categories[key].text)}
+                    onClick={activateCategoryHandler}
+                  />
+                  {categories[key].text}
+                </label>
+              </li>
+            ))
+          ) : (
             <LoadingIcon
               iconSize="text-2xl"
               text="loading"
               textSize="text-lg"
             />
-        )}
-      </ul>
+          )}
+        </ul>
+      </div>
     </>
   );
 }
