1b26f30d0944751628c9541bb5490ba07fb9d077
Victor R <victorvjro91@gmail.com>
2024-01-02 21:29:29 -0500

    CHNG: search filter bar styling

--- src/pages/search/SearchFilterBar.jsx
+++ src/pages/search/SearchFilterBar.jsx
@@ -16,15 +16,18 @@ export default function Default({ activeCategoriesState }) {
 
   return (
     <>
-      <div className="sticky top-[10px] mx-auto my-[20px] flex h-[50px] w-[calc(100%-70px)] max-w-[1200px] rounded-lg border-2 border-black bg-neutral-900">
-        <div className="group relative flex cursor-pointer items-center justify-center gap-1 rounded-l-lg bg-black px-3">
-          <FontAwesomeIcon icon={faFilter} className="text-2xl" />
-          <span className="text-lg font-bold">Filter</span>
+      <div className="sticky top-[10px] mx-auto my-[20px] flex h-[50px] w-[calc(100%-70px)] max-w-[1200px] [&_*]:border-black">
+        <div>
+          <button className="peer relative flex h-full items-center justify-center gap-1 rounded-l-lg border-2 bg-neutral-900 px-3">
+            <FontAwesomeIcon icon={faFilter} className="text-2xl" />
+            <span className="text-lg font-bold">Filter</span>
+          </button>
+
           <SortingCheckboxes activeCategoriesState={activeCategoriesState} />
         </div>
 
         <button
-          className="basic-button palet-red! rounded-none"
+          className="basic-button palet-red! rounded-none border-y-2"
           onClick={clearActiveCategoriesHandler}
         >
           Clear
