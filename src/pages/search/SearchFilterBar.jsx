fd019b31439e520653299bfaa7c7e4d7297d4470
Victor R <victorvjro91@gmail.com>
2024-01-04 22:14:44 -0500

    CHNG: search filter button styles

--- src/pages/search/SearchFilterBar.jsx
+++ src/pages/search/SearchFilterBar.jsx
@@ -17,8 +17,8 @@ export default function Default({ activeCategoriesState }) {
   return (
     <>
       <div className="sticky top-[10px] mx-auto my-[20px] flex h-[50px] w-[calc(100%-70px)] max-w-[1200px]">
-        <div>
-          <button className="peer relative flex h-full items-center justify-center gap-1 rounded-l-lg bg-neutral-900 px-3">
+        <div className="group">
+          <button className="relative flex h-full items-center justify-center gap-1 rounded-l-lg bg-neutral-900 px-3 transition-all delay-150 group-hover:rounded-bl-none group-hover:brightness-150 group-hover:delay-0">
             <FontAwesomeIcon icon={faFilter} className="text-2xl" />
             <span className="text-lg font-bold">Filter</span>
           </button>
