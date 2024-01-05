1b1d70ee6073df6488747ddf31107cfeb5bb2c8c
Victor R <victorvjro91@gmail.com>
2024-01-04 22:13:08 -0500

    CHNG: sorting checkboxes styling

--- src/pages/search/SortingCheckboxes.jsx
+++ src/pages/search/SortingCheckboxes.jsx
@@ -35,21 +35,23 @@ export default function Default({ activeCategoriesState }) {
 
   return (
     <>
-      <div className="absolute left-0 top-[calc(100%-10px)] z-[-1] hidden flex-col overflow-hidden rounded-b-lg bg-neutral-500 pb-1 pt-[10px] hover:flex peer-hover:flex">
+      <div className="clipPath-top-flat hover:clipPath-full group-hover:clipPath-full absolute left-0 top-full z-[-1] flex flex-col rounded-b-lg bg-neutral-600 pb-1 transition-all">
         <span className="mx-2 text-sm font-bold">Most have:</span>
         <ul className="contents">
           {categories?.length ? (
             Object.keys(categories).map((key) => (
-              <li key={key} className="contents">
-                <label className="inline cursor-pointer whitespace-nowrap px-2 hover:bg-black">
+              <li key={key} className="contents select-none">
+                <label className="group/label flex cursor-pointer items-center gap-1 whitespace-nowrap px-2 hover:bg-orange-500">
                   <input
                     type="checkbox"
                     name={categories[key].text}
-                    className="mr-1"
+                    className="peer flex h-[15px] w-[15px] appearance-none overflow-hidden rounded-sm bg-white p-[2px] after:h-full after:w-full after:rounded-sm after:transition-all after:duration-75 checked:after:bg-neutral-800 group-hover/label:after:bg-neutral-400 group-hover/label:checked:after:bg-neutral-800"
                     checked={activeCategories.includes(categories[key].text)}
                     onClick={activateCategoryHandler}
                   />
-                  {categories[key].text}
+                  <span className="group-hover/label:text-white peer-checked:text-neutral-400 peer-checked:line-through group-hover/label:peer-checked:text-neutral-200">
+                    {categories[key].text}
+                  </span>
                 </label>
               </li>
             ))
